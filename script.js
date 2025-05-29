4e05b2ec9352a289d81db02e45eec2c6c057fecb
IFCAR Solutions <dsi.ifcar@gmail.com>
2025-05-29 10:41:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,83 +15,26 @@ document.addEventListener('DOMContentLoaded', function() {
             nombrePoste: '1'
         },
         {
-            title: 'Responsable Marketing',
-            company: 'GlobalTech Solutions',
+            title: 'Cadre Gestionnaire de Facturation',
+            company: 'ANP',
+            location: 'Dakhla',
+            domaine: 'Gestion, Finances, Comptabilité, Logistique, Commerce ou équivalent.',
+            formation: 'Bac +5',
+            experience: 'Debutant',
+            nombrePoste: '1',
+        },
+        {
+            title: 'Cadre Planification',
+            company: 'Société Marocaine d’Ingénierie Touristique',
             location: 'Rabat',
-            domaine: 'Marketing',
-            formation: 'Bac +5 en Commerce',
-            experience: '5 ans minimum',
+            domaine: 'Statistique/ IA / data et/ou analyse financière',
+            formation: 'Ingénieur',
+            experience: 'De 1 à 2 ans',
             nombrePoste: '1',
-            description: 'Nous recherchons un responsable marketing créatif pour diriger nos campagnes marketing. Plus de détails ici...',
-            applyLink: 'https://www.facebook.com'
-        },
-        {
-            title: 'Spécialiste du Support Client',
-            company: 'Zeta Services',
-            location: 'Marrakech',
-            domaine: 'Service Client',
-            formation: 'Bac +2 en Communication',
-            experience: '1 ans minimum',
-            nombrePoste: '3',
-            description: 'Fournir un excellent support client et résoudre efficacement les demandes. Plus de détails ici...',
-            applyLink: 'https://www.linkedin.com'
-        },
-        {
-            title: 'Data Scientist',
-            company: 'Data Insights',
-            location: 'Casablanca',
-            domaine: 'Science des Données',
-            formation: 'Doctorat en Statistiques',
-            experience: '5 ans minimum',
-            nombrePoste: '1',
-            description: 'Rejoignez une équipe de recherche de pointe travaillant sur des algorithmes avancés. Plus de détails ici...',
-            applyLink: 'https://www.example.com/datascientist'
-        },
-        {
-            title: 'Infirmier(ère)',
-            company: 'Clinique Espoir',
-            location: 'Agadir',
-            domaine: 'Santé',
-            formation: 'Diplôme d\'État d\'Infirmier',
-            experience: '2 ans minimum',
-            nombrePoste: '5',
-            description: 'Offrez des soins de qualité et un soutien empathique aux patients dans un environnement bienveillant. Plus de détails ici...',
-            applyLink: 'https://www.example.com/nurse'
-        },
-        {
-            title: 'Enseignant(e) de Français',
-            company: 'École Lumière',
-            location: 'Rabat',
-            domaine: 'Éducation',
-            formation: 'Licence de Français',
-            experience: '3 ans minimum',
-            nombrePoste: '2',
-            description: 'Transmettez votre passion pour la langue française à des élèves motivés. Plus de détails ici...',
-            applyLink: 'https://www.example.com/teacher'
-        },
-         {
-            title: 'Développeur Full-Stack',
-            company: 'Code Wizards',
-            location: 'Tanger',
-            domaine: 'Informatique',
-            formation: 'Bac +5 en Informatique',
-            experience: '4 ans minimum',
-            nombrePoste: '3',
-            description: 'Nous recherchons un développeur talentueux capable de gérer le front-end et le back-end de nos applications. Plus de détails ici...',
-            applyLink: 'https://www.example.com/fullstack'
-        },
-        {
-            title: 'Community Manager',
-            company: 'Social Boost',
-            location: 'Marrakech',
-            domaine: 'Marketing Digital',
-            formation: 'Bac +3 en Marketing',
-            experience: '2 ans minimum',
-            nombrePoste: '1',
-            description: 'Animez nos réseaux sociaux et développez notre communauté en ligne. Plus de détails ici...',
-            applyLink: 'https://www.example.com/communitymanager'
-        },
 
+        },
+        
+         
     ];
 
     function createJobCard(job, index) {
